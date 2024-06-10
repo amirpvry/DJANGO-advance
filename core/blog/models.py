b50c5ae0e7cf77fd729c945a32eb9031ce60adfb
amirpvry <amirmahdi.alipooor@gmail.com>
2024-06-10 02:00:48 -0700

    branch ended

--- core/blog/models.py
+++ core/blog/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth import get_user_model
 # Create your models here.
 
-User = get_user_model()
+# User = get_user_model()
 
 class Post(models.Model):
     
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE)
 
     title = models.CharField(max_length= 256)
 
