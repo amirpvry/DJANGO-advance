9867ba83bf02f27492fc9493c5e94bf2cb502ff1
amirpvry <amirmahdi.alipooor@gmail.com>
2024-07-12 03:39:31 -0700

    titttr

--- core/accounts/api/v1/serialaizers.py
+++ core/accounts/api/v1/serialaizers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
 from accounts.models import User
+from django.core import exceptions
+from django.contrib.auth.password_validation import validate_password
+
+
 
 class RecursionSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(max_length = 255 , write_only = True)
 
     class Meta:
         model = User
-        fields = ['email' , 'password']
+        fields = ['email' , 'password' , 'password2']
+
+    def validate(self, attrs):
+        if attrs.get('password') != attrs.get('password2'):
+            raise serializers.ValidationError({ "detail": "Passwords do not match."})
+    
+        try:
+             # validate the password and catch the exception
+           validate_password(attrs.get('password'))
+         
+         # the exception raised here is different than serializers.ValidationError
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError({'password':list(e.messages)})
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
